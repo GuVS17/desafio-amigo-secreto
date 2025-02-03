dac6958828d518e0a4d4b1af2afa27080e24f9dd
Gustavo Vieira <gustavovs0241@gmail.com>
2025-02-03 17:45:17 -0300

    Código do jogo

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+
+    if (nome == '' ) {
+        alert('Por favor, insira um nome');
+    } else {
+        amigos.push(nome);
+    }
+    limparCampo();
+}
+
+
+let lista = document.getElementById(listaAmigos)
+listaAmigos.innerHTML = ""
+
+for (let i = 0; i < amigos.length; i++) {
+    let li = document.createElement('li');    
+    li.textContent = amigos[i];               
+    lista.appendChild(li);
+}
+
+
+function amigoAleatorio() {
+    if (amigos.length > 0) {
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        return amigos[indiceAleatorio];
+    } else { 
+        alert('Nenhum amigo para sortear.');
+        return null;
+    }
+}
+
+function sortearAmigo() {
+    let amigo = amigoAleatorio();
+    if (amigo) {
+        let amigoSorteado = document.getElementById('resultado');
+        amigoSorteado.innerHTML = 'O amigo sorteado foi: ' + amigo;
+    }
+}
+
+
+
+
+function limparCampo() {
+    nome = document.querySelector('input')
+    nome.value = '';
+}
